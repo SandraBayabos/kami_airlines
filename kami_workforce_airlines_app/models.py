901dfd46d6e32a7798d465101bd299e7ca4490e3
sandrabayabos <sandra.bayabos@gmail.com>
2023-12-16 22:48:53 +0800

    models, views, urlpatters: added urlpattern for airlines, created maximum_flight_time property

--- kami_workforce_airlines_app/models.py
+++ kami_workforce_airlines_app/models.py
@@ -12,3 +12,7 @@ class Airplane(models.Model):
     @property
     def fuel_consumption_per_minute(self):
         return log(self.airplane_id * 0.8) + self.passenger_count * 0.002
+    
+    @property
+    def max_flight_time(self):
+        return self.fuel_tank_capacity / self.fuel_consumption_per_minute
