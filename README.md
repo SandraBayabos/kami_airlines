1acbcea3dc3bfcd09e9ebd57073389277a28e078
sandrabayabos <sandra.bayabos@gmail.com>
2023-12-17 21:29:26 +0800

    tested /get and /post methods

---
+++ README.md
@@ -0,0 +1,46 @@
+#KAMI Airlines
+
+##Requirements:
+
+- Python3
+- Pip3
+- Python environment (e.g. `conda`, `venv`, `pyenv` etc.)
+- Postgresql
+
+Follow these steps to get your environment set up:
+
+1. Clone the repository:
+
+```
+git clone https://github.com/your-username/your-project-name.git
+cd kami_airlines
+
+```
+
+2. Set up a virtual environment (the following example uses `conda`)
+
+```
+conda create --name kami_airlines
+conda activate kami_airlines
+```
+
+3. Install the packages as per the requirements.txt file
+
+```
+pip3 install -r requirements.txt
+```
+
+4. Create the database
+
+```
+python3 manage.py create_db
+python3 manage.py makemigrations kami_workforce_airlines_app
+python3 manage.py migrate
+
+```
+
+5. To run the server
+
+```
+python3 manage.py runserver
+```
--- kami_workforce_airlines_app/tests.py
+++ kami_workforce_airlines_app/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from .models import Airplane
 from django.test import TestCase
+import json
 
 class AirplaneTestCase(TestCase):
     def setUp(self):
@@ -16,12 +17,25 @@ class AirplaneTestCase(TestCase):
     def test_airplane_fuel_consumption_per_minute(self):
         airplane1 = Airplane.objects.get(airplane_id=1)
         airplane2 = Airplane.objects.get(airplane_id=2)
-        self.assertEqual(airplane1.fuel_consumption_per_minute, 0.1)
-        self.assertEqual(airplane2.fuel_consumption_per_minute, 0.6)
+        self.assertEqual(airplane1.fuel_consumption_per_minute, -0.02)
+        self.assertEqual(airplane2.fuel_consumption_per_minute, 0.87)
         
     def test_airplane_max_flight_time(self):
         airplane1 = Airplane.objects.get(airplane_id=1)
         airplane2 = Airplane.objects.get(airplane_id=2)
-        self.assertEqual(airplane1.max_flight_time, 2000)
-        self.assertEqual(airplane2.max_flight_time, 666.67)
+        self.assertEqual(airplane1.max_flight_time, -10000.0)
+        self.assertEqual(airplane2.max_flight_time, 459.77)
+        
+    def test_get_all_airplanes(self):
+        response = self.client.get('/airlines/api/airplanes/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 2)
 
+    def test_create_airplane(self):
+        data = json.dumps([{'airplane_id': 3, 'passenger_count': 300}])
+        response = self.client.post('/airlines/api/airplanes/', data, content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data[0]['airplane_id'], 3)
+        self.assertEqual(response.data[0]['passenger_count'], 300)
+        self.assertEqual(response.data[0]['fuel_consumption_per_minute'], 1.48)
+        self.assertEqual(response.data[0]['max_flight_time'], 405.41)
