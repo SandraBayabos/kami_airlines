c9e3209588754c49dabf5191d12da263b8229d8e
sandrabayabos <sandra.bayabos@gmail.com>
2023-12-17 22:30:29 +0800

    override existing save to validate properties cannot be negative or too small

--- kami_workforce_airlines_app/models.py
+++ kami_workforce_airlines_app/models.py
@@ -3,7 +3,8 @@ from math import log
 
 
 class Airplane(models.Model):
-    airplane_id = models.IntegerField(primary_key=True, unique=True)  # user defined id
+    airplane_id = models.IntegerField(
+        primary_key=True, unique=True)  # user defined id
     passenger_count = models.IntegerField()  # user defined passenger assumption
 
     @property
@@ -17,3 +18,16 @@ class Airplane(models.Model):
     @property
     def max_flight_time(self):
         return round(self.fuel_tank_capacity / self.fuel_consumption_per_minute, 2)
+
+    def save(self, *args, **kwargs):
+        if self.airplane_id <= 0:
+            raise ValueError("airplane_id must be positive")
+        if self.passenger_count <= 0:
+            raise ValueError("passenger_count must be positive")
+        if self.fuel_consumption_per_minute <= 0:
+            raise ValueError(
+                "fuel_consumption_per_minute must be positive. Ensure your passenger_count and airplane_id values are not too small.")
+        if self.max_flight_time <= 0:
+            raise ValueError(
+                "max_flight_time must be positive. Ensure your passenger_count and airplane_id values are not too small.")
+        super().save(*args, **kwargs)
