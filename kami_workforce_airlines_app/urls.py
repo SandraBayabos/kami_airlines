efc5b29e37be0aa2deb59da7875dc6b742cdf2bf
sandrabayabos <sandra.bayabos@gmail.com>
2023-12-18 12:07:55 +0800

    code cleanup

--- kami_workforce_airlines_app/urls.py
+++ kami_workforce_airlines_app/urls.py
@@ -6,8 +6,4 @@ urlpatterns = [
     path('api/airplanes/', views.AirplaneAPIView.as_view(), name='airplanes'),
     path('api/airplanes/<int:airplane_id>/',
          views.AirplaneAPIView.as_view(), name='manage_airplane'),
-    # path('api/airplanes/<int:airplane_id>/',
-    #      views.AirplaneAPIView.as_view(), name='update_airplane'),
-    # path('api/airplanes/<int:airplane_id>/delete/',
-    #      views.AirplaneAPIView.as_view(), name='delete_airplane'),
 ]
