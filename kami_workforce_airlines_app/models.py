38e588106847d3da7255d111e58c8df00d41d932
sandrabayabos <sandra.bayabos@gmail.com>
2023-12-18 10:37:50 +0800

    models: if fuel consumption is 0, return none for maximum flight time, set minimum fuel_consumption_per_minute to a value above 0 to avoid unrealistic negative values

--- kami_workforce_airlines_app/models.py
+++ kami_workforce_airlines_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from math import log
 
+from django.forms import ValidationError
+
 
 class Airplane(models.Model):
     airplane_id = models.IntegerField(
@@ -13,27 +15,32 @@ class Airplane(models.Model):
 
     @property
     def fuel_consumption_per_minute(self):
-        return round(log(self.airplane_id * 0.8) + self.passenger_count * 0.002, 2)
+        result = log(self.airplane_id * 0.8) + self.passenger_count * 0.002
+        # set minimum fuel consumption to 0.01 to avoid unrealistic negative values of fuel consumption
+        return round(max(result, 0.01), 2)
 
     @property
     def max_flight_time(self):
+        if self.fuel_consumption_per_minute == 0:
+            return None
         return round(self.fuel_tank_capacity / self.fuel_consumption_per_minute, 2)
 
     def save(self, *args, **kwargs):
-        if self.airplane_id <= 1:
+        if self.airplane_id < 1:
             raise ValueError("airplane_id must be greater than 1")
         if self.passenger_count is None:
-            self.passenger_count = 1
+            raise ValueError("passenger_count cannot be null")
         if self.passenger_count <= 0:
             raise ValueError("passenger_count must be positive")
         self._recalculate_fuel_consumption_per_minute_and_max_flight_time()
 
         if self.fuel_consumption_per_minute <= 0 or self.max_flight_time <= 0:
-            raise ValueError(
-                "Calculated fuel consumption per minute and max flight time must be positive."
+            raise ValidationError(
+                "Calculated fuel consumption per minute and max flight time must be non-negative. "
+                "Please check your airplane ID and passenger count."
             )
         super().save(*args, **kwargs)
-        
+
     def __str__(self):
         return f"Airplane ID: {self.airplane_id}, Passenger Count: {self.passenger_count}"
 
