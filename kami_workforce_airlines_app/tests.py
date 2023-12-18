e6b04e158aafdd5d51067884adbea17cd68d45be
sandrabayabos <sandra.bayabos@gmail.com>
2023-12-18 10:41:52 +0800

    typing: added types into tests

--- kami_workforce_airlines_app/tests.py
+++ kami_workforce_airlines_app/tests.py
@@ -2,38 +2,39 @@ from django.test import TestCase
 from .models import Airplane
 from django.test import TestCase
 import json
+from typing import Dict, Any
 
 
 class AirplaneTestCase(TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         Airplane.objects.create(airplane_id=100, passenger_count=100)
         Airplane.objects.create(airplane_id=200, passenger_count=200)
 
-    def test_airplane_fuel_tank_capacity(self):
+    def test_airplane_fuel_tank_capacity(self) -> None:
         airplane1 = Airplane.objects.get(airplane_id=100)
         airplane2 = Airplane.objects.get(airplane_id=200)
         self.assertEqual(airplane1.fuel_tank_capacity, 20000)
         self.assertEqual(airplane2.fuel_tank_capacity, 40000)
 
-    def test_airplane_fuel_consumption_per_minute(self):
+    def test_airplane_fuel_consumption_per_minute(self) -> None:
         airplane1 = Airplane.objects.get(airplane_id=100)
         airplane2 = Airplane.objects.get(airplane_id=200)
         self.assertEqual(airplane1.fuel_consumption_per_minute, 4.58)
         self.assertEqual(airplane2.fuel_consumption_per_minute, 5.48)
 
-    def test_airplane_max_flight_time(self):
+    def test_airplane_max_flight_time(self) -> None:
         airplane1 = Airplane.objects.get(airplane_id=100)
         airplane2 = Airplane.objects.get(airplane_id=200)
         self.assertEqual(airplane1.max_flight_time, 4366.81)
         self.assertEqual(airplane2.max_flight_time, 7299.27)
 
-    def test_get_all_airplanes(self):
+    def test_get_all_airplanes(self) -> None:
         response = self.client.get('/airlines/api/airplanes/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data), 2)
 
-    def test_create_airplane(self):  # test create 10 airplanes
-        data = json.dumps([{'airplane_id': 300, 'passenger_count': 300}, {'airplane_id': 400, 'passenger_count': 400}, {'airplane_id': 500, 'passenger_count': 500}, {'airplane_id': 600, 'passenger_count': 600}, {'airplane_id': 700, 'passenger_count': 700}, {'airplane_id': 800, 'passenger_count': 800},
+    def test_create_airplane(self) -> None:  # test create 10 airplanes
+        data: str = json.dumps([{'airplane_id': 300, 'passenger_count': 300}, {'airplane_id': 400, 'passenger_count': 400}, {'airplane_id': 500, 'passenger_count': 500}, {'airplane_id': 600, 'passenger_count': 600}, {'airplane_id': 700, 'passenger_count': 700}, {'airplane_id': 800, 'passenger_count': 800},
                            {'airplane_id': 900, 'passenger_count': 900}, {'airplane_id': 1000, 'passenger_count': 1000}, {'airplane_id': 1100, 'passenger_count': 1100}, {'airplane_id': 1200, 'passenger_count': 1200}])
         response = self.client.post(
             '/airlines/api/airplanes/', data, content_type='application/json')
@@ -59,8 +60,8 @@ class AirplaneTestCase(TestCase):
         self.assertEqual(response.data[9]['airplane_id'], 1200)
         self.assertEqual(response.data[9]['passenger_count'], 1200)
 
-    def test_create_airplane_with_existing_airplane_id(self):
-        data = json.dumps([{'airplane_id': 100, 'passenger_count': 100},
+    def test_create_airplane_with_existing_airplane_id(self) -> None:
+        data: str = json.dumps([{'airplane_id': 100, 'passenger_count': 100},
                            {'airplane_id': 1300, 'passenger_count': 1300}])
         response = self.client.post(
             '/airlines/api/airplanes/', data, content_type='application/json')
@@ -72,8 +73,8 @@ class AirplaneTestCase(TestCase):
         )
         self.assertTrue(error_message_found)
 
-    def test_update_airplane(self):
-        data = {'passenger_count': 300}
+    def test_update_airplane(self) -> None:
+        data: Dict[str, Any] = {'passenger_count': 300}
         response = self.client.patch(
             '/airlines/api/airplanes/100/', json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, 200)
